396e77d38a588922ab2f91a12ce3d423d5c9272c
Nabila Bazar <102323948+nabilabazar@users.noreply.github.com>
2025-01-08 22:46:53 +0100

    Update AddProject.js
    Removed everything related to menu dropdown as the update and delete are moved back to the project.js file

--- src/services/project/AddProject.js
+++ src/services/project/AddProject.js
@@ -82,9 +82,9 @@ const AddProject = () => {
         navigate(`/project/${project.id}`);
     };
 
-    const handleMenuClick = (projectId) => {
-        setVisibleMenu(visibleMenu === projectId ? null : projectId);
-    };
+    // const handleMenuClick = (projectId) => {
+    //     setVisibleMenu(visibleMenu === projectId ? null : projectId);
+    // };
 
     return (
         <div className="containerNB">
@@ -102,15 +102,14 @@ const AddProject = () => {
                             className="project-itemNB"
                             onClick={() => handleProjectClick(project)}
                         >
-                            {/* Gradient Header with Project Name and Dots Menu */}
                             <div className="project-headerNB">
                                 <h4>{project.name}</h4>
-                                <img
-                                    src={dotsImage}
-                                    alt="Menu"
-                                    className="dots-menuNB"
-                                    onClick={() => handleMenuClick(project.id)} 
-                                />
+                                // <img
+                                //     src={dotsImage}
+                                //     alt="Menu"
+                                //     className="dots-menuNB"
+                                //     onClick={() => handleMenuClick(project.id)} 
+                                // />
                             </div>
 
                             <p className="project-descriptionNB">{project.description}</p>
@@ -118,15 +117,14 @@ const AddProject = () => {
                                 Created at: {new Date(project.createdAt).toLocaleDateString()}
                             </div>
 
-                            {/* Menu Dropdown (Initially hidden) */}
-                            {project.menuVisible && (
-                                <div className="menu-dropdownNB">
-                                    <ul>
-                                        <li>Edit Project</li>
-                                        <li>Delete Project</li>
-                                    </ul>
-                                </div>
-                            )}
+                            // {project.menuVisible && (
+                            //     <div className="menu-dropdownNB">
+                            //         <ul>
+                            //             <li>Edit Project</li>
+                            //             <li>Delete Project</li>
+                            //         </ul>
+                            //     </div>
+                            // )}
                         </div>
                     ))}
                 </div>
